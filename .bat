374d85d392ec5481141aed460a86856808f8202b
mclarasgalindo <mclarasgalindo@hotmail.com>
2025-02-13 15:18:53 -0300

    Add files via upload

---
+++ .bat
@@ -0,0 +1,18 @@
+mkdir galinhafrita
+mkdir guilhrmegalinhafrita
+mkdir saturnogalinha
+mkdir kitty
+pause
+rmdir saturnogalinha
+cd guilhrmegalinhafrita
+echo guilherme come galinha.>guilhrmegalinhafrita.txt
+cd ..
+rename galinhafrita sabrina
+move guilhrmegalinhafrita\guilhrmegalinhafrita.txt kitty\guilhermedoido.txt
+cd kitty
+copy guilhermedoido.txt sabrina.txt
+type sabrina.txt
+pause
+del sabrina.txt
+
+
